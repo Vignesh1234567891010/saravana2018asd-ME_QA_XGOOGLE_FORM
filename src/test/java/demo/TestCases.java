7c65ef9e22b9599104cf1557ceb1a7dbdcf9dba3
Vignesh <saravana2018asd@gmail.com>
2025-03-31 20:04:51 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,15 +1,20 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
@@ -21,6 +26,92 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    @Test(description = "Automate Google Form", enabled = true)
+    public void testCase01() throws InterruptedException {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        //Navigated to expected Url
+        driver.navigate().to("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+
+        wait.until(ExpectedConditions.urlToBe("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform"));
+
+        String currentUrl = Wrappers.getCurrentUrl(driver);
+
+        if(currentUrl.contains("forms")){
+            Assert.assertTrue(true);
+            System.out.println("Navigated to the google form");
+        }else {
+            Assert.assertFalse(false);
+            System.out.println("Unable to navigate to google form");
+        }
+
+        //Code to perform Name in field
+        WebElement name_field = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='i1']/following::input[1]")));
+        Thread.sleep(2000);
+        name_field.sendKeys("Crio Learner");
+
+        System.out.println("Typed name into input box");
+
+
+        //Code to perform answer in input box
+        Wrappers.shiftToNextTab(driver);
+        Wrappers.sendKeys(driver, "I want to be the best QA Engineer! 1743424670");
+
+        System.out.println("Typed phrase into input box");
+
+        //code to perform actions on radio button
+        Wrappers.shiftToNextTab(driver);
+        WebElement zero_to_two = driver.findElement(By.xpath("//*[@id='i16']/div[3]"));
+        zero_to_two.click();
+        System.out.println("Clicked on Experience box");
+
+        //Code to perform actions on checkBox
+        Wrappers.shiftToNextTab(driver);
+        WebElement javaCheck = driver.findElement(By.xpath("//label[@for='i34']"));
+        javaCheck.click();
+
+        WebElement seleniumCheck = driver.findElement(By.xpath("//label[@for='i37']"));
+        seleniumCheck.click();
+
+
+        WebElement testNGCheck = driver.findElement(By.xpath("//label[@for='i43']"));
+        testNGCheck.click();
+        System.out.println("Chose any of the Tools you are experienced in");
+
+        //Code to perform actions on dropdown
+        WebElement dropDown = driver.findElement(By.xpath("//div[@jsname='wQNmvb'][2]"));
+        Wrappers.click(driver, dropDown);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div[2]")));
+        Wrappers.downArrow(driver);
+        Wrappers.enter(driver);
+        Thread.sleep(2000);
+        System.out.println("Chose how to be addressed option");
+
+        //Code to perform action in simple calendar
+        Wrappers.shiftToNextTab(driver);
+        Wrappers.sendKeys(driver, "24032025");
+        System.out.println("Entered 7 days before date");
+
+        //Code to perform actions on time input field
+        Wrappers.shiftToNextTab(driver);
+        Wrappers.shiftToNextTab(driver);
+        Wrappers.sendKeys(driver, "07");
+        Wrappers.shiftToNextTab(driver);
+        Wrappers.sendKeys(driver, "30");
+        Thread.sleep(2000);
+        System.out.println("Entered correct time into time input");
+        // Code to perform submit form
+        WebElement submitButton = driver.findElement(By.xpath("//span[text()='Submit']"));
+        Wrappers.click(driver, submitButton);
+
+        //Code to get Success Message
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']")));
+        String successMessage = driver.findElement(By.xpath("//div[text()='Thanks for your response, Automation Wizard!']")).getText();
+        Assert.assertEquals(successMessage, "Thanks for your response, Automation Wizard!");
+        System.out.println("Read the text at the end of submission: "+successMessage);
+
+    }
 
      
     /*
@@ -45,7 +136,7 @@ public class TestCases {
         System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
 
         driver = new ChromeDriver(options);
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -4,10 +4,11 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.checkerframework.checker.units.qual.A;
+import org.checkerframework.checker.units.qual.K;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -16,4 +17,38 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    public static String getCurrentUrl(WebDriver driver){
+        return driver.getCurrentUrl();
+    }
+
+    public static void shiftToNextTab(WebDriver driver){
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.TAB).keyUp(Keys.TAB).perform();
+    }
+
+    public static void sendKeys(WebDriver driver, String message){
+        Actions actions = new Actions(driver);
+        actions.sendKeys(message).perform();
+    }
+
+    public static void click(WebDriver driver, WebElement element){
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click().perform();
+    }
+
+    public static void enter(WebDriver driver){
+
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.ENTER).keyDown(Keys.ENTER).perform();
+    }
+
+    public static void downArrow(WebDriver driver){
+
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.ARROW_DOWN).keyUp(Keys.ARROW_UP).perform();
+    }
+
+
 }
